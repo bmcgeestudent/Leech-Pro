3959b62fa1433de718b37b13c64e4da9f287d17b
bmcgeestudent <83534601+bmcgeestudent@users.noreply.github.com>
2021-07-12 19:31:03 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -79,7 +79,7 @@ async def youtube_dl_call_back(bot, update):
     # https://superuser.com/a/994060
     LOGGER.info(custom_file_name)
     #
-    await update.message.edit_caption(caption="**DownloadinG.... please wait..**")
+    await update.message.edit_caption(caption="**Downloading.... please wait..**")
 
     tmp_directory_for_each_user = os.path.join(
         DOWNLOAD_LOCATION, str(update.message.message_id)
@@ -156,7 +156,7 @@ async def youtube_dl_call_back(bot, update):
         return False, None
     if t_response:
         dir_contents = len(os.listdir(tmp_directory_for_each_user))
-        await update.message.edit_caption(caption=f"found {dir_contents} files")
+        await update.message.edit_caption(caption=f"Found {dir_contents} files")
         user_id = update.from_user.id
         #
         LOGGER.info(tmp_directory_for_each_user)

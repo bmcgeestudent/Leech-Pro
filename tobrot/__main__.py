01a6eccf156741478086e64bd7b4984b0d24308d
bmcgeestudent <83534601+bmcgeestudent@users.noreply.github.com>
2021-07-16 17:04:42 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -42,6 +42,7 @@ from tobrot import (
     GPYTDL_COMMAND,
     TOGGLE_VID,
     TOGGLE_DOC,
+    HELP_COMMAND,
 )
 from tobrot.helper_funcs.download import down_load_media_f
 from tobrot.plugins.call_back_button_handler import button
@@ -68,6 +69,7 @@ from tobrot.plugins.status_message_fn import (
     upload_log_file,
     upload_as_doc,
     upload_as_video,
+    help_message_f,
 )
 
 if __name__ == "__main__":
@@ -245,5 +247,12 @@ if __name__ == "__main__":
         filters=filters.command([f"{TOGGLE_VID}"]) & filters.chat(chats=AUTH_CHANNEL), 
     )
     app.add_handler(upload_as_video_handler)
+    #
+    help_message_handler = MessageHandler(
+        help_message_f,
+        filters=filters.command([f"{HELP_COMMAND}"])
+        & filters.chat(chats=AUTH_CHANNEL),
+    )
+    app.add_handler(help_message_handler)
     #
     app.run()
